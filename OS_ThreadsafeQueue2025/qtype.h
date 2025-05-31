24aed967f0b749b30c71d636d62bed74f8ba315a
Yeonha Park <frog3oh35@gmail.com>
2025-05-31 20:03:25 +0900

    초기 커밋.

---
+++ OS_ThreadsafeQueue2025/qtype.h
@@ -0,0 +1,35 @@
+#ifndef _QTYPE_H // header guard
+#define _QTYPE_H
+
+
+// ==========이 파일은 수정 가능==========
+
+typedef unsigned int Key; //값이 클수록 높은 우선순위
+typedef void* Value;
+
+typedef struct {
+	Key key;
+	Value value;
+} Item;
+
+typedef struct {
+	bool success; // true : 성공, false : 실패
+	Item item;
+	//필드 추가 가능
+} Reply;
+
+typedef struct node_t {
+	Item item;
+	struct node_t* next;
+	//필드 추가 가능
+} Node;
+
+typedef struct {
+	Node* head, * tail;
+	//필드 추가 가능
+} Queue;
+
+//자료형 정의, 추가/수정 가능
+
+
+#endif
