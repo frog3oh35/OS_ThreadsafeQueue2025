ebfd5b58e642487dfb06d6a0baf95a4422ed3986
Yeonha Park <frog3oh35@gmail.com>
2025-06-03 00:40:50 +0900

    [feat] release() 구현

--- OS_ThreadsafeQueue2025/queue.cpp
+++ OS_ThreadsafeQueue2025/queue.cpp
@@ -15,7 +15,21 @@ Queue* init(void) {
 }
 
 void release(Queue* queue) {
-	return;
+	
+	//인자로 받은 큐가 NULL이면 할 거 없음
+	if (queue == NULL) return;
+	
+	//현재 위치를 head부터 시작해서, 끝날때까지 (NULL 까지) 반복
+	Node* cur = queue->head;
+	while (cur != NULL) {
+		// ↓다음 노드를 미리 저장하기 (현재 노드 날리면 next 못 감)
+		Node* next = cur->next;
+		//현재 노드 메모리에서 날리기! 이하 반복
+		nfree(cur);
+		cur = next;
+	}
+	//큐 자체도 delete로 날리기!
+	delete queue;
 }
 
 Node* nalloc(Item item) {
